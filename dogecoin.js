ce35694df2de74ab553e9cf069ec0466953e242c
steveyout <34503856+steveyout@users.noreply.github.com>
2018-10-26 23:16:57 +0300

    Add files via upload

--- dogecoin.js
+++ dogecoin.js
@@ -1,3 +1,4 @@
+
 const Telegraf = require('telegraf');
 const Extra = require('telegraf/extra');
 const Markup = require('telegraf/markup');
@@ -6,13 +7,17 @@ const Scene = require('telegraf/scenes/base')
 const session = require('telegraf/session')
 const Stage = require('telegraf/stage')
 const { enter,leave } = Stage
+
+var cron = require('node-cron');
 var mysql = require('mysql');
 var WAValidator = require('wallet-address-validator');
-var mysecret = '8eDpUW9PJ7E16xlns9msu5vUNxth9G0A'
-var mykey = 'JaH2VY37PArRPeod'
-var Client = require('coinbase').Client;
-var client = new Client({'apiKey': mykey, 'apiSecret': mysecret});
-var acc='d63b2e5d-4e54-5990-943f-ef5788433df1'
+var mysecret = 'C94C02f742e0F91fD5A8e2676F1d499c6d648df18Ba42fC78E31ac94a1909E0e'
+var mykey = '46c505f7f82a5b9d80adfbdde80e09a3883635cfc82b7dda56ceb29935db460c'
+var Coinpayments = require('coinpayments');
+const client = new Coinpayments({
+    key: mykey,
+    secret: mysecret
+});
 var con = mysql.createConnection({
     host: "bgdiheuar-mysql.services.clever-cloud.com",
     user: "uko7zijkihlgnqne",
@@ -57,7 +62,8 @@ bot.command('start',ctx => {
         var tim = new Date();
         var address = 'none';
         var power = 0;
-        var user = {id: chatid, balance: bal, firstname: firstname, time: tim, withdrawadd: address, power: power};
+        var refa=411002680;
+        var user = {id: chatid, balance: bal, firstname: firstname, time: tim, withdrawadd: address, power: power,ref:refa};
         con.query("insert into `account` SET ?", user, function (error, results) {
             ctx.reply('welcome' + ctx.from.first_name + '\n\n✨🔥 Easiest and most reliable dogecoin Mine Bot for Telegram!\n\n⚡️ Automatic payments!', Markup
                 .keyboard([
@@ -79,54 +85,157 @@ bot.command('start',ctx => {
 
             .resize()
             .extra())
-    }else if (message.text.split(start)[1] !== id) {
-        var chatd = message.text.split(start)[1]
-        con.query("SELECT ref FROM account WHERE id=" + chatd, function (err, result, fields) {
-            console.log(result[0].ref)
-            if (result[0].ref <= 0) {
-                var refbonus = 25;
-                var ref = 1;
-                var refid = message.text.split(start)[1];
-                var sql = "update `account` set `power` =power+ '" + refbonus + "', friends =friends+ " +
-                    ref + ", ref = " + refid + " where `id` = '" + refid + "'";
-                con.query(sql)
-                console.log(err)
-                ctx.reply('welcome' + ctx.from.first_name + '\n\n✨🔥 Easiest and most reliable dogecoin Mine Bot for Telegram!\n\n⚡️ Automatic payments!', Markup
-                    .keyboard([
-                        ['👤ACCOUNT'], // Row1 with 2 buttons
-                        ['✨POWER', '💵PAYMENTS', '👨‍👧‍👦REFFERALS'], // Row2 with 2 buttons
-                        ['🌎LANGUAGE', '❓ABOUT US'] // Row3 with 3 buttons Row3 with 3 buttons
-                    ])
+    } else if (message.text.split(start)[1] !== id) {
 
-                    .resize()
-                    .extra())
-                con.query("SELECT id FROM account WHERE id=" + refid, function (err, result, fields) {
-                    ctx.telegram.sendMessage(result[0].id, 'you have a new refferal\nyou receive:+25🌟')
+        var chatd = ctx.from.id
+        con.query("SELECT id FROM account WHERE id=" + chatd, function (err, result, fields) {
+            console.log(result.length)
+            if (result.length==0) {
+
+                var chatidi = ctx.from.id;
+                var firstnamee = ctx.from.first_name;
+                var bala = 0;
+                var time = new Date();
+                var addresse = 'none';
+                var powere = 0;
+                var refidi = message.text.split(start)[1]
+                var useri = {
+                    id: chatidi,
+                    balance: bala,
+                    firstname: firstnamee,
+                    time: time,
+                    withdrawadd: addresse,
+                    power: powere,
+                    ref: refidi
+                };
+                con.query("insert into `account` SET ?", useri)
+
+                var chatd = ctx.from.id
+                con.query("SELECT ref FROM account WHERE id=" + chatd, function (err, result, fields) {
+
+                    if (result[0].ref !== refidi) {
+                        var refbonus = 25;
+                        var ref = 1;
+                        var refid = message.text.split(start)[1];
+
+                        var sql = "update `account` set `power` =`power`+" + refbonus + ", `friends`=`friends`+ '" + ref + "' where `id` = '" + refid + "'";
+
+                        con.query(sql)
+
+                        ctx.reply('welcome' + ctx.from.first_name + '\n\n✨🔥 Easiest and most reliable dogecoin Mine Bot for Telegram!\n\n⚡️ Automatic payments!', Markup
+                            .keyboard([
+                                ['👤ACCOUNT'], // Row1 with 2 buttons
+                                ['✨POWER', '💵PAYMENTS', '👨‍👧‍👦REFFERALS'], // Row2 with 2 buttons
+                                ['🌎LANGUAGE', '❓ABOUT US'] // Row3 with 3 buttons Row3 with 3 buttons
+                            ])
+
+                            .resize()
+                            .extra())
+                        con.query("SELECT id FROM account WHERE id=" + refid, function (err, result, fields) {
+                            ctx.telegram.sendMessage(result[0].id, 'you have a new refferal\nyou receive:+25 Mh/s HASHPOWER')
 
 
+                        })
+                    }
                 })
-            } else if (result[0].ref==ctx.message.text.split(start)[1]) {
-                ctx.reply('🚫you have already used this link', Markup
-                    .keyboard([
-                        ['👤ACCOUNT'], // Row1 with 2 buttons
-                        ['✨POWER', '💵PAYMENTS', '👨‍👧‍👦REFFERALS'], // Row2 with 2 buttons
-                        ['🌎LANGUAGE', '❓ABOUT US'] // Row3 with 3 buttons Row3 with 3 buttons
-                    ])
 
-                    .resize()
-                    .extra())
+
+
+
+
+
+
+
+                    }else if (result.length>0) {
+                var rd=ctx.from.id
+                con.query("SELECT ref FROM account WHERE id=" + rd, function (err, result, fields) {
+                     if (result[0].ref == ctx.message.text.split(start)[1]) {
+                        ctx.reply('🚫you have already used this link', Markup
+                            .keyboard([
+                                ['👤ACCOUNT'], // Row1 with 2 buttons
+                                ['✨POWER', '💵PAYMENTS', '👨‍👧‍👦REFFERALS'], // Row2 with 2 buttons
+                                ['🌎LANGUAGE', '❓ABOUT US'] // Row3 with 3 buttons Row3 with 3 buttons
+                            ])
+
+                            .resize()
+                            .extra())
+                    }
+                })
             }
         })
     }
 })
 
+
+
+
+
+
+
 bot.hears('👨‍👧‍👦REFFERALS',ctx => {
     const id = ctx.from.id
     con.query("SELECT friends FROM account WHERE id=" + id, function (err, result, fields) {
-        ctx.reply('invite friends and get 25mhs for each friend\n your refferal link is:http://t.me/doge_testerbot?start=' + id + '\n\n invited refferals:' + result[0].friends+' 👥'+'\n\nearned from refferals: '+result[0].friends*25+'mhs')
+        ctx.reply('invite friends and get 25 Mh/s HASHPOWER for each friend and 25% of their deposits\n your refferal link is:http://t.me/doge_testerbot?start=' + id + '\n\n invited refferals:' + result[0].friends+' 👥'+'\n\nearned from refferals: '+result[0].friends*25+'Mh/s ')
 
     })
 })
+
+////Balance
+
+
+bot.hears('👤ACCOUNT',ctx => {
+    var chatid = ctx.from.id;
+    con.query("SELECT balance,firstname,power FROM account WHERE id=" + chatid, function (err, result, fields) {
+        ctx.reply('👤your account ' + ctx.from.first_name + '\n\n✨Power: ' + result[0].power + ' Mh/s'+'\n💰Balance: ' + result[0].balance / 100000000 + ' Ð')
+
+
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//cron
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+cron.schedule('*/50 * * * * *', () => {
+    console.log('every 1 second')
+    var bonus=1000;
+    var bala =0;
+    var sql = "update `account` set `balance` =`balance`+" + bonus + ", `trail`=`trail`+ '" + bonus + "' where `balance` >= '" + bala + "'";
+    con.query(sql)
+});
+
+
+
+
+
+
+
 
 
 bot.startPolling()
